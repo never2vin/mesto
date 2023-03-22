72e9ec0ab4d8b6dad0374df4ee240e1e5d8f9f13
Alex Strelin <stres@live.ru>
2023-03-22 22:24:48 +0300

    feat: добавить 6 карточек при загрузки страницы

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,47 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const elementTemplate = document.getElementById('element-template').content;
+const elementsList = document.querySelector('.elements');
+
+initialCards.forEach(renderElement);
+
+function renderElement (item) {
+  const htmlElement = elementTemplate.cloneNode(true);
+
+  const elementImage = htmlElement.querySelector('.element__image');
+  elementImage.src = item.link;
+  elementImage.alt = item.name;
+
+  htmlElement.querySelector('.element__title').textContent = item.name;
+
+  elementsList.append(htmlElement);
+}
+
 const profileElement = document.querySelector('.profile');
 const profileEditButtonElement = profileElement.querySelector('.profile__edit-button');
 const profileNameElement = profileElement.querySelector('.profile__name');
