8f3e7c065dbd323fcf81ed16fc64cc2045b1484e
Alex Strelin <stres@live.ru>
2023-04-29 15:09:49 +0300

    ref: добавить в класс Card элементы карточки как поля класса

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,7 +1,6 @@
 export default class Card {
   constructor(data, templateSelector) {
-    this._link = data.link;
-    this._title = data.name;
+    this._data = data;
     this._templateSelector = templateSelector;
     this._popupElement = document.querySelector('.popup_type_image');
   }
@@ -17,38 +16,40 @@ export default class Card {
   }
 
   _setEventListeners() {
-    this._element.querySelector('.element__image').addEventListener('click', this._handleOpenPopup);
-    this._element.querySelector('.element__like-icon').addEventListener('click', this._toggleLikeIconActivity);
-    this._element.querySelector('.element__trash-icon').addEventListener('click', this._removeCardElement);
+    this._imageElement.addEventListener('click', this._handleOpenPopup);
+    this._likeIconElement.addEventListener('click', this._toggleLikeIconActivity);
+    this._cardElement.querySelector('.element__trash-icon').addEventListener('click', this._removeCardElement);
   }
 
-  _toggleLikeIconActivity() {
-    this.classList.toggle('element__like-icon_active');
+  _toggleLikeIconActivity = () => {
+    this._likeIconElement.classList.toggle('element__like-icon_active');
   }
 
   _removeCardElement = () => {
-    this._element.remove();
+    this._cardElement.remove();
   }
 
   _handleOpenPopup = () => {
-    this._popupElement.querySelector('.popup__image').src = this._link;
-    this._popupElement.querySelector('.popup__image').alt = this._title;
+    this._popupElement.querySelector('.popup__image').src = this._data.link;
+    this._popupElement.querySelector('.popup__image').alt = this._data.title;
 
-    this._popupElement.querySelector('.popup__caption').textContent = this._title;
+    this._popupElement.querySelector('.popup__caption').textContent = this._data.title;
 
     this._popupElement.classList.add('popup_opened');
   }
 
   generateCard() {
-    this._element = this._getTemplate();
+    this._cardElement = this._getTemplate();
 
-    this._element.querySelector('.element__image').src = this._link;
-    this._element.querySelector('.element__image').alt = this._title;
+    this._imageElement = this._cardElement.querySelector('.element__image');
+    this._likeIconElement = this._cardElement.querySelector('.element__like-icon');
 
-    this._element.querySelector('.element__title').textContent = this._title;
+    this._cardElement.querySelector('.element__title').textContent = this._data.name;
+    this._imageElement.src = this._data.link;
+    this._imageElement.alt = this._data.name;
 
     this._setEventListeners();
 
-    return this._element;
+    return this._cardElement;
   }
 }
