f4bc44440686f93a999fb628d2ca17d51ce31ca2
Alex Strelin <stres@live.ru>
2023-05-14 22:27:48 +0300

    refactor: перенести передачу данных картинки в метод open класса PopupWithImage

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -2,18 +2,16 @@ import Popup from "./Popup.js";
 
 export default class PopupWithImage extends Popup {
 
-  constructor({name, link}, popupSelector) {
+  constructor(popupSelector) {
     super(popupSelector);
 
-    this._name = name;
-    this._link = link;
     this._popupImage = this._popup.querySelector('.popup__image');
     this._popupCaption = this._popup.querySelector('.popup__caption');
 }
 
-  open() {
-    this._popupImage.src = this._link;
-    this._popupImage.alt = this._name;
+  open({name, link}) {
+    this._popupImage.src = link;
+    this._popupImage.alt = name;
 
     this._popupCaption.textContent = this._name;
 
